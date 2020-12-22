d731a997b285a9965e64ceb44398b00474de4927
guy-pc <sadoun90@gmail.com>
2020-12-22 11:47:13 +0200

    simulator for poisson people arrive with lambda(input) to vaccinated. service is poisson with mu (input). output by order: vaccinated people arrived people last vaccine gave time no people in hospital A_ti - avg of i people waiting Z_0 - probability for no people want to vaccinate Z_i - probability for i people want to vaccinate T_w - avg waiting time T_s - avg service time mean_lambda - avg arrival rate (lambda)

--- main.py
+++ main.py
@@ -1,145 +1,108 @@
-import numpy as np
-import pandas as pd
 import sys
-from scipy.stats import poisson
-
-#global
-TOTAL_TIME, NUM_OF_VACCINES, LAMBDA_ARRIVAL, MU_SERVICE, PROBABILITIES, QUEUE_LEN = 0, 0, 0, 0, [], 0
-
-def gen_int_arr():  # function to generate arrival times using inverse trnasform
-    return -np.log((np.random.uniform(low=0.0, high=1.0))) / LAMBDA_ARRIVAL
-
-
-def gen_service_time_teller1():  # function to generate service time for teller 1 using inverse trnasform
-    return -np.log((np.random.uniform(low=0.0, high=1.0))) / MU_SERVICE
-
-
-def gen_service_time_teller2():  # function to generate service time for teller 1 using inverse trnasform
-    return -np.log((np.random.uniform(low=0.0, high=1.0))) / MU_SERVICE
-
-
-def choose_vaccine():
-    return np.random.randint(low=0, high=NUM_OF_VACCINES - 1)
-
-
-def gen_prob():  # function to generate service time for teller 1 using inverse trnasform
-    return np.random.uniform(low=0.0, high=1.0)
+import numpy as np
+import random as rand
 
 
 class Simulation:
-    def __init__(self):
+    def __init__(self, T, M, Lambda, Mu, probs):
+        self.total_time = T
+        self.num_of_vaccines = M
+        self.lambda_arrival = Lambda
+        self.mu_service = Mu
+        self.probabilities = probs
+        self.q_len = len(probs)
+
         self.clock = 0.0  # simulation clock
+        self.customers_in_q = 0  # customers in queue - First in q gets service
         self.num_arrivals = 0  # total number of arrivals
-        self.t_arrival = gen_int_arr()  # time of next arrival
-        self.t_departure = float('inf')  # departure time from server
-        self.num_in_q = 0  # current number in queue
-        self.states = np.zeros(shape=NUM_OF_VACCINES)  # current state of servers (binary)
-        self.current_q_id = choose_vaccine()  # vaccine chosen by costumers/current queue
-
-        self.dep_sum = np.zeros(shape=NUM_OF_VACCINES)  # Sum of service times
-        self.num_of_departures = np.zeros(shape=NUM_OF_VACCINES)  # number of customers served
-        self.total_wait_time = 0.0  # total wait time
-        self.waited_in_queue = 0  # customers who had to wait in line(counter)
+        self.t_arrival = self.gen_next_arrival()  # time of next arrival
+        self.t_departure = float('inf')  # time of next departure
+        self.queues_len_time = np.zeros(shape=self.q_len, dtype=float)  # current state of each station
+        self.current_q_id = self.choose_vaccine()  # vaccine chosen by costumers/current queue
         self.lost_customers = 0  # customers who left without service
+        self.t_last_vaccine = 0  # time of last vaccine given
 
-        # self.t_departure1 = float('inf')  # departure time from server 1
-        # self.t_departure2 = float('inf')  # departure time from server 2
-        # self.dep_sum1 = 0  # Sum of service times by teller 1
-        # self.dep_sum2 = 0  # Sum of service times by teller 2
-        # self.state_T1 = 0  # current state of server1 (binary)
-        # self.state_T2 = 0  # current state of server2 (binary)
-        # self.num_of_departures1 = 0  # number of customers served by teller 1
-        # self.num_of_departures2 = 0
+    def simulate(self):
+        while self.clock <= self.total_time:
+            self.time_adv()
+        self.print_result()
 
     def time_adv(self):
         t_next_event = min(self.t_arrival, self.t_departure)
-        self.total_wait_time += (self.num_in_q * (t_next_event - self.clock))
+        t_waiting = (t_next_event - self.clock)
         self.clock = t_next_event
-
-        if self.t_arrival < self.t_departure:
+        self.queues_len_time[self.customers_in_q] += t_waiting
+        if self.clock == self.t_arrival:
             self.arrival()
         else:
             self.depart()
 
     def arrival(self):
         self.num_arrivals += 1
-
-        if self.num_in_q == 0:  # schedule next departure or arrival depending on state of servers
-            self.current_q_id = choose_vaccine()
-            if self.states[self.current_q_id] == 0:
-                self.states[self.current_q_id] = 1
-                t_service = gen_service_time_teller1()
-                self.dep_sum[self.current_q_id] += t_service
-                self.t_departure = self.clock + t_service
-                self.t_arrival = self.clock + gen_int_arr()
-            else:
-                self.num_in_q += 1
-                self.waited_in_queue += 1
-                self.t_arrival = self.clock + gen_int_arr()
-        elif 1 <= self.num_in_q < QUEUE_LEN:
-            p = gen_prob()
-            if p <= PROBABILITIES[self.num_in_q]:
-                print(self.num_in_q)
-                self.num_in_q += 1
-                self.waited_in_queue += 1
-                self.t_arrival = self.clock + gen_int_arr()
+        self.t_arrival += self.gen_next_arrival()
+        if self.customers_in_q == 0:  # no one is waiting or getting service
+            self.current_q_id = self.choose_vaccine()
+            self.customers_in_q += 1
+            self.t_departure = self.clock + self.gen_service_time()
+        elif self.customers_in_q < self.q_len:
+            assert self.customers_in_q > 0
+            prob = np.random.uniform(low=0.0, high=1.0)
+            if prob < self.probabilities[self.customers_in_q]:
+                self.customers_in_q += 1
             else:
                 self.lost_customers += 1
-        else:
-            self.lost_customers += 1
 
-    def depart(self):  # departure from server 2
-        self.num_of_departures[self.current_q_id] += 1
-        if self.num_in_q > 0:
-            t_service = gen_service_time_teller1()
-            self.dep_sum[self.current_q_id] += t_service
-            self.t_departure = self.clock + t_service
-            self.num_in_q -= 1
+    def depart(self):
+        self.t_last_vaccine = self.clock
+        self.customers_in_q -= 1
+        if self.customers_in_q > 0:
+            self.t_departure = self.clock + self.gen_service_time()
         else:
             self.t_departure = float('inf')
-            self.states[self.current_q_id] = 0
+
+    def print_result(self):
+        served = self.num_arrivals - self.lost_customers
+        total_waiting_time = sum((self.queues_len_time[x]*(x-1)) for x in range(2, self.q_len))
+        total_service_time = sum(self.queues_len_time[1:])
+        total_time = sum(self.queues_len_time)
+        X, Y, T, T0 = served, self.lost_customers, self.t_last_vaccine, self.queues_len_time[0]
+        T_w = total_waiting_time / served
+        T_s = total_service_time / served
+        lambda_A = served / total_time
+        print(f'{X} {Y} {"%.2f" % T} {"%.3f" % self.queues_len_time[0]}', end=' ')
+        for i in range(1, self.q_len):  # print A_Ti's including A_T0
+            print(f'{"%.3f" % (self.queues_len_time[i] / self.num_of_vaccines)}', end=' ')
+        print(f'{"%.6f" % (self.queues_len_time[0] / total_time)}', end=' ')
+        for i in range(1, self.q_len):  # print Z_Ti's
+            print(f'{"%.6f" % ((self.queues_len_time[i] / total_time) / self.num_of_vaccines)}', end=' ')
+        print(f'{"%.7f" % T_w} {"%.7f" % T_s} {"%.2f" % lambda_A}')
+        # print(f'X: {X} Y: {Y} T\': {"%.2f" % T}', end=' ')
+        # print(f'T_0: {"%.3f" % self.queues_len_time[0]}', end=' ')
+        # for i in range(1, self.q_len):  # print A_Ti's including A_T0
+        #     print(f'T_{i}: {"%.3f" % (self.queues_len_time[i]/self.num_of_vaccines)}', end=' ')
+        # print(f'Z_0: {"%.6f" % (self.queues_len_time[0]/total_time)}', end=' ')
+        # for i in range(1, self.q_len):  # print Z_Ti's
+        #     print(f'Z_{i}: {"%.6f" % ((self.queues_len_time[i]/total_time)/self.num_of_vaccines)}', end=' ')
+        # print(f'T_w: {"%.7f" % T_w}', end=' ')
+        # print(f'T_s: {"%.7f" % T_s}', end=' ')
+        # print(f'Lambda_mean: {"%.2f" % lambda_A}')
+
+    def gen_next_arrival(self):  # function to generate arrival times using inverse transform
+        return rand.expovariate(self.lambda_arrival)
+
+    def gen_service_time(self):
+        return rand.expovariate(self.mu_service)
+
+    def choose_vaccine(self):
+        return np.random.randint(low=0, high=self.num_of_vaccines)
 
 
 def main():
-    args = sys.argv[1:]
-    global TOTAL_TIME, NUM_OF_VACCINES, LAMBDA_ARRIVAL, MU_SERVICE, PROBABILITIES, QUEUE_LEN
-    TOTAL_TIME = int(args[0])
-    NUM_OF_VACCINES = int(args[1])
-    LAMBDA_ARRIVAL = int(args[2])
-    MU_SERVICE = int(args[3])
-    PROBABILITIES = np.array(args[4:], dtype=float)
-    QUEUE_LEN = len(PROBABILITIES)
-    print(
-        f'Time: {TOTAL_TIME},Num of vaccuines: {NUM_OF_VACCINES}, Lambda: {LAMBDA_ARRIVAL}, Mu: {MU_SERVICE}, Probs: {PROBABILITIES}, queue len: {QUEUE_LEN}')  # Press Ctrl+F8 to toggle the breakpoint.
-    s = Simulation()
-    s.__init__()
-    while s.clock <= TOTAL_TIME:
-        s.time_adv()
-    a = pd.Series([s.clock / s.num_arrivals, s.waited_in_queue, s.total_wait_time, s.lost_customers])
-    print(a)
-    #     df = df.append(a, ignore_index=True)
-    # df = pd.DataFrame(
-    #     columns=['Average interarrival time', 'Average service time teller1', 'Average service time teller 2',
-    #              'Utilization teller 1', 'Utilization teller 2', 'People who had to wait in line',
-    #              'Total average wait time', 'Lost Customers'])
-    #
-    # for i in range(100):
-    #     np.random.seed(i)
-    #     s.__init__()
-    #     while s.clock <= 240:
-    #         s.time_adv()
-    #     a = pd.Series([s.clock / s.num_arrivals, s.dep_sum1 / s.num_of_departures1, s.dep_sum2 / s.num_of_departures2,
-    #                    s.dep_sum1 / s.clock, s.dep_sum2 / s.clock, s.number_in_queue, s.total_wait_time,
-    #                    s.lost_customers], index=df.columns)
-    #     df = df.append(a, ignore_index=True)
-    #
-    # df.to_excel('results.xlsx')
-    # LAMBDA_ARRIVAL = 1 / float(args.lambdaArrival)
-    # MU_SERVICE = 1 / float(args.muService)
-    # TOTAL_ARRIVALS = int(args.totalArrivals)
-    #
-    # sim = QueueSimulator(TOTAL_ARRIVALS, LAMBDA_ARRIVAL, MU_SERVICE)
-    # sim.run()
+    # args = sys.argv[1:]
+    args = [1000, 2, 60, 30, 1, 0.8, 0.5, 0]
+
+    s = Simulation(float(args[0]), int(args[1]), float(args[2]), float(args[3]), np.array(args[4:], dtype=float))
+    s.simulate()
 
 
 if __name__ == '__main__':
